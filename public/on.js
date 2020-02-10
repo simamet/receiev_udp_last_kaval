20a0478f16417460f73ea66bf78257b0ae54f229
simamet <simammet@gmail.com>
2020-02-10 16:02:24 +0700

    Add checkbox

--- public/on.js
+++ public/on.js
@@ -59,3 +59,17 @@ button.addEventListener('click', function(e) {
   console.log('button was clicked');
 
 });
+
+function myFunction() {
+  // Get the checkbox
+  var checkBox = document.getElementById("myCheck");
+  // Get the output text
+  var text = document.getElementById("text");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+} 
